cedcc0f2b84fc2340cd9fde6ba90a3f94b048ef7
kotafan1rich <lesapond2007@gmail.com>
2022-11-10 16:06:45 +0300

    3.8

--- create_bot.py
+++ create_bot.py
@@ -7,7 +7,7 @@ from aiogram.dispatcher import Dispatcher
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('TOKEN') # '5439011304:AAGF_03sJSmc8GR7gkUhjG_3SEvadUPz3oI'
+TOKEN = os.getenv('TOKEN') # '5439011304:AAGF_03sJSmc8GR7gkUhjG_3SEvadUPz3oI' '5463053983:AAHe8kj5ahBXqRfuF-XuzJn4qZr0bqyjNtM'
 storage = MemoryStorage()
 
 bot = Bot(token=TOKEN)
